52e68eeb00262d890b6eacc371b355498f52ec05
Mahdi Mazaheri <mahdi.mazaheri8383@gmail.com>
2023-05-28 11:40:21 +0330

    add cart endpoint to Constant.java

--- src/main/java/com/example/store_front/Constant.java
+++ src/main/java/com/example/store_front/Constant.java
@@ -3,4 +3,6 @@ package com.example.store_front;
 public class Constant {
     public final static String API_END_POINT = "http://localhost:8080/";
     public final static String PRODUCT_API_END_POINT = API_END_POINT + "product";
+    public final static String CART_API_END_POINT = API_END_POINT + "cart";
+    public final static String ORDER_API_END_POINT = API_END_POINT + "order";
 }
