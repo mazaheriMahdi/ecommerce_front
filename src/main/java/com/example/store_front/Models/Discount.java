a5ca6d84e397f3fb8b4135ad45b65ffafee3a82a
Mahdi Mazaheri <mahdi.mazaheri8383@gmail.com>
2023-06-06 01:54:11 +0330

    create discount and discount request model

---
+++ src/main/java/com/example/store_front/Models/Discount.java
@@ -0,0 +1,4 @@
+package com.example.store_front.Models;
+
+public record Discount( Long id, String code,String userName , int percent , int count , int dayLeft ) {
+}
---
+++ src/main/java/com/example/store_front/Models/RequestModel/DiscountRequestModel.java
@@ -0,0 +1,3 @@
+package com.example.store_front.Models.RequestModel;
+
+public record DiscountRequestModel(int percent , int count , int day , String email) {}
