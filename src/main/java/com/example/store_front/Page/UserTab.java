d417c351f29cc5873cb95cf884102d98598647fb
Mahdi Mazaheri <mahdi.mazaheri8383@gmail.com>
2023-06-06 17:39:19 +0330

    user tab for admin page

---
+++ src/main/java/com/example/store_front/Page/UserTab.java
@@ -0,0 +1,33 @@
+package com.example.store_front.Page;
+
+import com.example.store_front.Components.CustomAlert;
+import com.example.store_front.Components.UserCard;
+import com.example.store_front.Models.User;
+import com.example.store_front.Service.User.UserService;
+import javafx.geometry.Pos;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+
+public class UserTab extends BorderPane {
+    public UserTab() {
+        super();
+        VBox root = new VBox();
+        root.getStyleClass().add("darkPrimaryBack");
+        ScrollPane scrollPane = new ScrollPane(root);
+        scrollPane.setFitToHeight(true);
+        scrollPane.setFitToWidth(true);
+        try {
+            UserService.getAll().forEach(user -> {
+                root.getChildren().add(new UserCard(user));
+            });
+        } catch (IOException | InterruptedException e) {
+            CustomAlert customAlert = new CustomAlert("Error");
+            customAlert.show();
+        }
+        root.setAlignment(Pos.CENTER);
+        this.setCenter(scrollPane);
+    }
+}
