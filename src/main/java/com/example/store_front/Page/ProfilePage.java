4954fd64b20b9f4f0d1a3a0264f358169a31c434
Mahdi Mazaheri <mahdi.mazaheri8383@gmail.com>
2023-06-07 01:19:08 +0330

    add discount list to ProfilePage

--- src/main/java/com/example/store_front/Page/ProfilePage.java
+++ src/main/java/com/example/store_front/Page/ProfilePage.java
@@ -1,11 +1,15 @@
 package com.example.store_front.Page;
 
 import com.example.store_front.Components.TextFieldWithLabel;
+import com.example.store_front.Service.Discount.DiscountService;
 import com.example.store_front.Service.User.UserService;
 import de.jensd.fx.glyphs.GlyphsDude;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
@@ -47,12 +51,28 @@ public class ProfilePage extends VBox {
             this.getScene().getWindow().hide();
         });
 
-
-
-
+        VBox vBox = new VBox();
+        vBox.setAlignment(Pos.CENTER);
+        vBox.setSpacing(20);
+        try {
+            DiscountService.getAllDiscounts().forEach(
+                    discount -> {
+                        vBox.getChildren().add(new Text(discount.toString()));
+                    }
+            );
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        ScrollPane scrollPane = new ScrollPane(vBox);
+        scrollPane.setFitToWidth(true);
+        scrollPane.setFitToHeight(true);
+        vBox.getStyleClass().add("darkPrimaryBack");
+        vBox.setAlignment(Pos.CENTER);
+        vBox.setPadding(new Insets(10));
+        scrollPane.setPadding(new Insets(-1));
         this.setAlignment(Pos.CENTER);
         this.setSpacing(20);
-        this.getChildren().addAll(name, email, submit ,back);
+        this.getChildren().addAll(name, email, submit ,back , scrollPane);
 
     }
 }
